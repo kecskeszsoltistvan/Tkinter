79e55f93c5688166c314e82ac920ec1b83bb4b76
kecskeszsoltistvan <78853814+kecskeszsoltistvan@users.noreply.github.com>
2022-03-23 13:19:19 +0100

    Add files via upload

--- Órai munka2.py
+++ Órai munka2.py
@@ -1,7 +1,24 @@
 from tkinter import *
+from tkinter import ttk
 import math
+
+def teszt():
+    
+    radius = int(r.get())
+    mass = int(m.get())
+    V = radius ** 2 * mass * math.pi
+    Vas = V * 7,874
+    Vfa = V * 0.650
+    v.delete(0, END)
+    v.insert(0, V)
+    v1.delete(0, END)
+    v1.insert(0, Vas)
+    v2.delete(0, END)
+    v2.insert(0, Vfa)
+    return None
+
 ablak1 = Tk()
-ablak1.title("IKT Projekt - Vashenger- 2022.03.22")
+ablak1.title("IKT Projekt - Vashenger - 2022.03.22")
 ablak1.geometry('220x140')
 ablak1.maxsize(220, 140)
 ablak1.minsize(220, 140)
@@ -38,18 +55,21 @@ Label(
     bg='#eeeeee'
 ).grid(row=5, column=0)
 
-r = Entry(ablak1).grid(row=0, column=1)
-m = Entry(ablak1).grid(row=1, column=1)
+r = ttk.Entry(ablak1)
+r.grid(row=0, column=1)
+m = ttk.Entry(ablak1)
+m.grid(row=1, column=1)
 
-def teszt():
-    v.delete(0, END)
-    return None
 
-gomb = Button(ablak1, text ="Hello", ).grid(row=2, column=1)
-Valami = "asd"
-v = Entry(ablak1,state='disabled',textvariable=Valami).grid(row=3, column=1)
-v1 = Entry(ablak1,state='disabled',textvariable=Valami).grid(row=4, column=1)
-v2 = Entry(ablak1,state='disabled',textvariable=Valami).grid(row=5, column=1)
+
+gomb = Button(ablak1, text = "Kiszámít", command = teszt).grid(row=2, column=1, sticky = "e")
+
+v = Entry(ablak1)
+v.grid(row=3, column=1)
+v1 = Entry(ablak1)
+v1.grid(row=4, column=1)
+v2 = Entry(ablak1)
+v2.grid(row=5, column=1)
 ablak1.mainloop()
 """
 Órai munka IG
